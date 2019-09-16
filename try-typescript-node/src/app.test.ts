6ee875bcf0c41e9f375c06d985560522b8b489dc
Ian Homer <ian.homer@gmail.com>
2019-09-16 21:03:14 +0100

    Linting

--- try-typescript-node/src/app.test.ts
+++ try-typescript-node/src/app.test.ts
@@ -1,18 +1,19 @@
-import { expect } from 'chai';
-const request = require('supertest');
+import { expect } from "chai";
+const request = require("supertest");
 
 import app from "./app";
 
-describe('Hello', () => {
-    describe('#GET / hello', () => {
-        it('should get hello', (done) => {
-            request(app).get('/')
-                .end((err: any, response: { statusCode: any; body: any; }) => {
-                    expect(response.statusCode).to.equal(200);
-                    expect(response.body).to.be.an('object');
-                    expect(response.body.message).to.equal('Hello World!');
-                    done();
-                });
+describe("Hello", () => {
+  describe("#GET / hello", () => {
+    it("should get hello", done => {
+      request(app)
+        .get("/")
+        .end((err: any, response: { statusCode: any; body: any }) => {
+          expect(response.statusCode).to.equal(200);
+          expect(response.body).to.be.an("object");
+          expect(response.body.message).to.equal("Hello World!");
+          done();
         });
     });
+  });
 });
--- try-typescript-node/src/app.ts
+++ try-typescript-node/src/app.ts
@@ -1,34 +1,32 @@
-import express = require('express');
+import express = require("express");
 import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 
 class App {
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
 
-    constructor() {
-        this.app = express();
-        this.config();
-        this.routes();
-    }
+  public app: express.Application;
 
-    public app: express.Application;
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
 
-    private config(): void {
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-    }
+  private routes(): void {
+    const router = express.Router();
 
-    private routes(): void {
-        const router = express.Router();
+    router.get("/", (req: Request, res: Response) => {
+      res.status(200).send({
+        message: "Hello World!"
+      });
+    });
 
-        router.get('/', (req: Request, res: Response) => {
-            res.status(200).send({
-                message: 'Hello World!'
-            })
-        });
-        
-        this.app.use('/', router)
-
-    }
+    this.app.use("/", router);
+  }
 }
 
 export default new App().app;
