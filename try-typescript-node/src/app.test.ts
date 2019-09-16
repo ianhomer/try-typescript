a77dd9a675fc919f662ab43ebe779c024052818f
Ian Homer <ian.homer@gmail.com>
2019-09-16 21:01:07 +0100

    Add eslint

--- try-typescript-node/src/app.test.ts
+++ try-typescript-node/src/app.test.ts
@@ -3,11 +3,11 @@ const request = require('supertest');
 
 import app from "./app";
 
-describe('Hello', function() {
-    describe('#GET / hello', function() {
-        it('should get hello', function(done) {
+describe('Hello', () => {
+    describe('#GET / hello', () => {
+        it('should get hello', (done) => {
             request(app).get('/')
-                .end(function(err: any, response: { statusCode: any; body: any; }) {
+                .end((err: any, response: { statusCode: any; body: any; }) => {
                     expect(response.statusCode).to.equal(200);
                     expect(response.body).to.be.an('object');
                     expect(response.body.message).to.equal('Hello World!');
