692a1def5401d1398593047705b5dfa03e332f4e
Ian Homer <code@ianhomer.com>
2022-12-18 09:43:38 +0000

    ✅ change from mocha to jest

--- packages/try-typescript-scratch/src/core/enum.test.ts
+++ packages/try-typescript-scratch/src/core/enum.test.ts
@@ -1,18 +1,16 @@
-import { expect } from 'chai';
-
 import { Color, ConstEnum, EnumFromConst } from './enum';
 
 describe('Enums', () => {
   it('should enums be OK', () => {
     const color = Color.Red;
-    expect(color).to.be.equal(Color.Red);
+    expect(color).toBe(Color.Red);
   });
   it('should const enums be OK', () => {
     const value = ConstEnum.BAR;
-    expect(value).to.be.equal(ConstEnum.BAR);
+    expect(value).toBe(ConstEnum.BAR);
   });
   it('should const enum from const be OK', () => {
     const value: EnumFromConst = 'FOO';
-    expect(value).to.be.equal('FOO');
+    expect(value).toBe('FOO');
   });
 });
--- packages/try-typescript-scratch/src/hello.test.ts
+++ packages/try-typescript-scratch/src/hello.test.ts
@@ -1,12 +1,8 @@
-import 'mocha';
-
-import { expect } from 'chai';
-
 import { hello } from './hello';
 
 describe('Hello function', () => {
   it('should return hello world', () => {
     const result = hello();
-    expect(result).to.equal('Hello world!');
+    expect(result).toBe('Hello world!');
   });
 });
