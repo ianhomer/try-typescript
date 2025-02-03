10e11427ee499039b2f667f04cecd1628c4821e5
Ian Homer <code@ianhomer.com>
2025-02-03 18:21:22 +0000

    📦️ upgrade packages

--- packages/try-typescript-react/src/index.tsx
+++ packages/try-typescript-react/src/index.tsx
@@ -1,9 +1,9 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 
 import Hello from "./components/Hello";
 
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById("example"),
+const root = ReactDOM.createRoot(
+  document.getElementById("example") as HTMLElement,
 );
+root.render(<Hello compiler="TypeScript" framework="React" />);
